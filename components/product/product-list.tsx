e86c9cbfc02c0a442797d9957da409a326d5e605
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-07-13 21:59:38 +0700

    product detail style

--- components/product/product-list.tsx
+++ components/product/product-list.tsx
@@ -9,7 +9,7 @@ type ProductListProps = {
 function ProductList(props: ProductListProps) {
   const { products } = props
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+    <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8 pt-5">
       {products.map(product => (
         <ProductListItem key={product.id} {...product} />
       ))}
--- pages/products/[pid].tsx
+++ pages/products/[pid].tsx
@@ -56,33 +56,35 @@ function ProductDetailPage(props: ProductDetailPageProps) {
   }
 
   return (
-    <div className='flex'>
-      <img src={product.thumbnail} alt={product.title} className='h-72 w-72 object-cover' />
-      <div>
-        <p className="text-xl pt-4 px-4 font-semibold">
-          {product.title}
-        </p>
-        <p className="text-sm p-4 text-grey-600 font-light">
-          {product.description}
-        </p>
-        <p className="text-palette-dark font-primary font-medium text-base absolute bottom-0 right-0 mb-4 pl-8 pr-4 pb-1 pt-2 bg-palette-lighter 
-            rounded-tl-sm triangle">
-          {product.price}
-        </p>
-        <div className='flex pl-4 mb-5'>
-          <label className='mr-2'>Qty:</label>
-          <input type="number" ref={qtyRef} className='form-input border border-grey-300 pl-1' defaultValue={1} />
+    <div className='flex flex-col justify-center items-center pt-12'>
+      <div className='flex lg:space-x-8'>
+        <div className='h-70 w-82'>
+          <img src={product.thumbnail} alt={product.title} className='object-cover' />
         </div>
-        <div className='flex flex-col pl-4'>
-          <button 
-            className='pt-3 pb-2 mb-2 rounded-sm text-lg bg-palette-primary text-white font-primary font-semibold' 
-            onClick={handleAddToCart}>Add to cart</button>
-          <Link
-            className='pt-3 pb-2 rounded-sm text-lg font-primary font-semibold text-center text-palette-primary border border-palette-primary' 
-            href="/">See all products</Link>
+        <div className='w-72'>
+          <p className="text-xl pt-4 px-4 font-semibold">
+            {product.title}
+          </p>
+          <p className="text-sm p-4 text-grey-600 font-light">
+            {product.description}
+          </p>
+          <p className="text-palette-dark font-primary font-medium text-base mb-4 pl-8 pr-4 pb-1 pt-2">
+            {product.price}
+          </p>
+          <div className='flex pl-4 mb-5'>
+            <label className='mr-2'>Qty:</label>
+            <input type="number" ref={qtyRef} className='form-input border border-grey-300 pl-1' defaultValue={1} />
+          </div>
+          <div className='flex flex-col pl-4'>
+            <button 
+              className='pt-3 pb-2 mb-2 rounded-sm text-lg bg-palette-primary text-white font-primary font-semibold' 
+              onClick={handleAddToCart}>Add to cart</button>
+            <Link
+              className='pt-3 pb-2 rounded-sm text-lg font-primary font-semibold text-center text-palette-primary border border-palette-primary' 
+              href="/">See all products</Link>
+          </div>
         </div>
       </div>
-      
     </div>
   )
 }
