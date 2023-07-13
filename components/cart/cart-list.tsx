3634f34e0fa52c24b8fea5178a4a3c5a5d8e9a87
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-07-14 06:10:06 +0700

    grand total

--- components/cart/cart-list.tsx
+++ components/cart/cart-list.tsx
@@ -5,59 +5,67 @@ import XMarkIcon from "../icons/x-mart-icon"
 function CartList() {
   const { cartList, updateCart } = useCart()
   return (
-    <table className="min-h-80 max-w-2xl mx-auto w-full">
-      <thead>
-        <tr className="uppercase text-xs sm:text-sm text-palette-primary border-b bolder-pallete-light text-center">
-          <td className="font-primary font-normal px-6 py-4">Product</td>
-          <td className="font-primary font-normal px-6 py-4">Qty</td>
-          <td className="font-primary font-normal px-6 py-4">Price</td>
-          <td className="font-primary font-normal px-6 py-4">Remove</td>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-palette-lighter">
-        {Object.values(cartList).map(({ product, qty, subtotal }) => (
-          <tr 
-            key={product.id}
-            className="text-sm sm:text-base text-gray-600"
-            >
-            <td className="font-primary font-medium px-4 py-4 flex items-center">
-              <img 
-                src={product.thumbnail}
-                width={64}
-                height={64}
-                className="mr-2"
-              />
-              <Link
-                className="pt-1"
-                href={`/products/${product.id}`}>{product.title}</Link>
-            </td>
-            <td className="font-primary font-medium px-4 py-4 sm:px-6 text-center">
-              <input 
-                type="number"
-                min="1"
-                step="1"
-                className="text-gray-900 form-input border border-gray-300 w-16 rounded-sm pl-1"
-                value={qty}
-                onChange={val => updateCart({ product, qty: Number(val.target.value) })}
-              />
-            </td>
-            <td
-              className="font-primary text-base font-light px-4 sm:px-6 py-4 hidden sm:table-cell text-center"
-            >
-              {subtotal || 0}
-            </td>
-            <td className="font-primary font-medium px-4 sm:px-6 py-4 text-center">
-              <button
-                onClick={() => updateCart({ product, qty: 0 })}>
-                <XMarkIcon
-                  className="w-8 h-8 text-palette-primary border border-palette-primary p-1 hover:bg-palette-lighter"
-                />
-              </button>
-            </td>
+    <>
+      <table className="min-h-80 max-w-2xl mx-auto w-full">
+        <thead>
+          <tr className="uppercase text-xs sm:text-sm text-palette-primary border-b bolder-pallete-light text-center">
+            <td className="font-primary font-normal px-6 py-4">Product</td>
+            <td className="font-primary font-normal px-6 py-4">Qty</td>
+            <td className="font-primary font-normal px-6 py-4">Price</td>
+            <td className="font-primary font-normal px-6 py-4">Remove</td>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="divide-y divide-palette-lighter">
+          {Object.values(cartList).map(({ product, qty, subtotal }) => (
+            <tr 
+              key={product.id}
+              className="text-sm sm:text-base text-gray-600"
+              >
+              <td className="font-primary font-medium px-4 py-4 flex items-center">
+                <img 
+                  src={product.thumbnail}
+                  width={64}
+                  height={64}
+                  className="mr-2"
+                />
+                <Link
+                  className="pt-1"
+                  href={`/products/${product.id}`}>{product.title}</Link>
+              </td>
+              <td className="font-primary font-medium px-4 py-4 sm:px-6 text-center">
+                <input 
+                  type="number"
+                  min="1"
+                  step="1"
+                  className="text-gray-900 form-input border border-gray-300 w-16 rounded-sm pl-1"
+                  value={qty}
+                  onChange={val => updateCart({ product, qty: Number(val.target.value) })}
+                />
+              </td>
+              <td
+                className="font-primary text-base font-light px-4 sm:px-6 py-4 hidden sm:table-cell text-center"
+              >
+                {subtotal || 0}
+              </td>
+              <td className="font-primary font-medium px-4 sm:px-6 py-4 text-center">
+                <button
+                  onClick={() => updateCart({ product, qty: 0 })}>
+                  <XMarkIcon
+                    className="w-8 h-8 text-palette-primary border border-palette-primary p-1 hover:bg-palette-lighter"
+                  />
+                </button>
+              </td>
+            </tr>
+          ))}
+          <tr className="text-center">
+            <td></td>
+            <td className="font-primary px-6 py-4 font-semibold uppercase text-base">Total</td>
+            <td className="font-primary text-palette-primary text-lg font-medium">{Object.values(cartList).reduce((acc, val) => (acc + (val.subtotal || 0)), 0)}</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </>
   )
 }
 
